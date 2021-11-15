5095a98e19ca86987bc4b76aa7305d54215d7075
Maria Eduarda <sabinomaariaeduarda123@gmail.com>
2021-11-14 21:40:12 -0300

    projeto

---
+++ backend/app/Controllers/Http/AuthController.ts
@@ -0,0 +1,16 @@
+ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class AuthController {
+    public async register({request}:HttpContextContract){
+      const data = request.only(["email", "password"])
+      const user = await User.create(data)
+      return user
+    }
+    public async login({ request, auth}:HttpContextContract ){
+        const{ email, password } = request.all()
+        const token = await auth.use('api').attempt(email, password)
+        const user = await User.findByOrFail("email", email)
+        return{ token, user }
+    }
+}
