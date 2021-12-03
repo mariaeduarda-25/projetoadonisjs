8df5aec1a1058a026e3912537a48a0baab313e23
Maria Eduarda <sabinomaariaeduarda123@gmail.com>
2021-12-02 21:49:56 -0300

    config

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -20,9 +20,9 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-//Route.get('/', async () => {
- // return { hello: 'world' }
-//})
+Route.get('/', async () => {
+  return { hello: 'world' }
+})
 
 Route.post("/register", "AuthController.register")
 Route.post("/login", "AuthController.login")
